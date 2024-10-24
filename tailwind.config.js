12ed38c0c0df994e4bdef4afe085aea046351cdf
Sening isming <Emailing>
2024-10-24 19:20:53 +0300

    css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,18 @@
 module.exports = {
   content: ["./public/**/*.{html,js}","./index.html"],
   theme: {
-    extend: {},
+    extend: {
+      textColor: {
+        "white-textColor": "white",
+        "blue-textColor": "#1a8ec1",
+        "green-textColor": "#50af31",
+        "pink-textColor": "#ff6699",
+        "yellow-textColor": "#fecc00",
+        "red-textColor": "#ed1c24",
+        "orange-textColor": "#ec6825",
+        "purple-textColor": "#a84d98",
+      }
+    },
   },
   plugins: [],
 }
